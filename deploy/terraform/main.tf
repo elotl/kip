f8be1f5130c9ecd263bf8e4599e759f95d314791
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-02-27 11:14:16 -0800

    Use Ubuntu 18.04

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -363,7 +363,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
   filter {
