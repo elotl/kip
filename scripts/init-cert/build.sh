575f0f072e095292608d3b604988bffdec38a800
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-06-15 09:57:58 -0700

    Build only one init-cert image
    Only build one init-cert image, with all supported kubectl versions.
    During runtime, a version of kubectl that is compatible with the server
    is chosen automatically.

--- scripts/init-cert/build.sh
+++ scripts/init-cert/build.sh
@@ -2,14 +2,21 @@
 
 set -euxo pipefail
 
+TAG=${TAG:-$(git describe --dirty)}
 DKR=${DKR:-docker}
-PUSH_IMAGES=${PUSH_IMAGES:-false}
+REPO=${REPO:-elotl/init-cert}
+PUSH_IMAGES=${PUSH_IMAGES:-true}
+UPDATE_LATEST=${UPDATE_LATEST:-false}
 
-major=1
-for minor in $(seq 12 18); do
-    tag="k8s-${major}.${minor}"
-    docker build -t elotl/init-cert:${tag} --build-arg K8S_VERSION_MAJOR=${major} --build-arg K8S_VERSION_MINOR=${minor} .
-    $PUSH_IMAGES && docker push elotl/init-cert:${tag} || true
-done
-docker tag elotl/init-cert:k8s-1.18 elotl/init-cert:latest
-$PUSH_IMAGES && docker push elotl/init-cert:latest || true
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
+docker build -t ${REPO}:${TAG} --build-arg K8S_VERSIONS="1.14 1.15 1.16 1.17 1.18" .
+
+if $PUSH_IMAGES; then
+    docker push ${REPO}:${TAG}
+    if $UPDATE_LATEST; then
+        docker tag ${REPO}:${TAG} ${REPO}:latest
+        docker push ${REPO}:latest
+    fi
+fi
