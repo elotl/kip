ef1dbec9beace05e7bdf2b3ab772c79e307a4719
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-06-26 16:08:32 -0700

    Fetch GPG key from keyserver
    Instead of fetching the key and then adding it to apt, which has been
    flaky lately, fetch it from a keyserver. This should help with smoke
    test flakyness.

--- deploy/terraform-aws/node.sh
+++ deploy/terraform-aws/node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -fL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A030B21BA07F4FB
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
