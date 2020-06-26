178405f382d106c8e9a464c676142579253556e7
Brendan Cox <bcox@elotl.co>
2020-06-26 11:38:07 -0700

    updating docs and manifests for deploying into GCE

--- deploy/terraform-gcp/variables.tf
+++ deploy/terraform-gcp/variables.tf
@@ -29,7 +29,7 @@ variable "service-cidr" {
 }
 
 variable "kustomize-dir" {
-  default     = "../manifests/kip/base"
+  default     = "../manifests/kip/overlays/gcp"
   description = "A kustomization directory that will be applied once the cluster is created. Leave it empty to disable."
 }
 
